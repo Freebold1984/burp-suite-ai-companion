98d862d2664a111a7435e59636e2c1c36a6e3874
FreeBoldSec <147902717+Freebold1984@users.noreply.github.com>
2025-07-31 16:11:40 -0500

    Add .next/static/chunks/src_app_burp-config_page_tsx_a21a6cd3._.js 1753996133737

---
+++ .next/static/chunks/src_app_burp-config_page_tsx_a21a6cd3._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_c9c3192c._.js",
+  "static/chunks/src_e04d0b7a._.js"
+],
+    source: "dynamic"
+});
